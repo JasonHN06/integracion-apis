cda39d1d3df758318d25d7b2849d326020cff432
Jason Madrid <jasonhnn6@gmail.com>
2026-05-28 14:34:04 -0600

    Cambiando estructura

---
+++ funciones-agregacion/backend/db/connection.js
@@ -0,0 +1,16 @@
+const { Sequelize } = require("sequelize");
+
+const sequelize = new Sequelize("graficos", "root", "123456789", {
+  host: "localhost",
+  port: 3306,
+  dialect: "mysql",
+});
+
+sequelize
+  .authenticate()
+  .then(() => console.log("Conexión exitosa"))
+  .catch((err) => {
+    console.error("Error de conexión:", err);
+  });
+
+module.exports = sequelize;
