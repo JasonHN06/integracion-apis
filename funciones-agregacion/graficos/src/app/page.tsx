04a23e1128b7215ad879f254b4d3e44ceb821a1b
Jason Madrid <jasonhnn6@gmail.com>
2026-05-29 19:45:07 -0600

    1er grafico agregado

--- funciones-agregacion/graficos/src/app/page.tsx
+++ funciones-agregacion/graficos/src/app/page.tsx
@@ -1,9 +1,13 @@
+import Link from "next/link";
 
 
 export default function Home() {
   return (
-    <div>
-      <h1>Gráficos</h1>
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+
+      <Link href="/TotalProductos" className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition-colors" >Total Productos</Link>
+      <Link href="/ValorTotalProductos" className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition-colors" >Valor Total de Productos</Link>
+      <Link href="/ValorTotalProductosPorTipo" className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition-colors" >Valor Total de Productos por Tipo</Link>
     </div>
   );
 }
